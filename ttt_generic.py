f7c3361d6ace2637a1bdb56a0d310540c1895a6a
sw2703 <30507015+sw2703@users.noreply.github.com>
2019-11-02 22:59:17 -0700

    Update ttt_generic.py
    Wrote random move engine. Need to write unit test.

--- ttt_generic.py
+++ ttt_generic.py
@@ -5,6 +5,8 @@ Created on Sat Nov  2 22:09:24 2019
 @author: daugh
 """
 
+import random
+
 class State:
     
     def __init__(self, board=[[0,0,0],[0,0,0],[0,0,0]], turn = 1):
@@ -16,4 +18,30 @@ class State:
         self.board = board
         self.turn = turn
         
+    def change_turn(self):
+        if self.turn == 1:
+            self.turn = 2
+        else:
+            self.turn = 1
+        
+class Engine:
     
+    """
+    For now, a random move engine. Will be expanded.
+    """
+    def move(self, state):
+        """
+        Input:
+            state: the current state
+        Returns:
+            A state, in which a move has been made
+        """
+        legal_positions = []
+        for x in range(3):
+            for y in range(3):
+                if state.board[x, y] == 0:
+                    legal_positions.append((x,y))
+        position = random.choice(legal_positions)
+        state.board[position] = state.turn
+        state.change_turn()
+        return state

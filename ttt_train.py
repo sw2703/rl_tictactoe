c0cddbff122981b09919dcc0f54ceb2c0f6d1969
sw2703 <30507015+sw2703@users.noreply.github.com>
2019-11-10 09:37:16 -0800

    Update ttt_train.py

--- ttt_train.py
+++ ttt_train.py
@@ -1,4 +1,5 @@
 from ttt_play import Game
+from ttt_policies import Policy, TabularPolicy
 
 class Train:
      def __init__(self, n_game, read_path = None, write_path):
@@ -13,4 +14,13 @@ class Train:
           self.SelfPlay()
                
      def SelfPlay(self):
+          """Policy Iteration following Sutton Barto 4.3
+          """
+          # 1. Initialization
+          policy_1 = TabularPolicy(has_v_dict = True)
+          policy_2 = TabularPolicy(has_v_dict = True)
           
+          # 2. Policy Evaluation
+          delta = 0
+          for _ in range(n_game):
+               

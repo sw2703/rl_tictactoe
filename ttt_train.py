d6a5a800190fb8aca4eb891c44c3ee91fe24b35a
sw2703 <30507015+sw2703@users.noreply.github.com>
2019-11-10 15:49:36 -0800

    Update ttt_train.py
    WIP. TODO: refactor, pull out judge() to be a method of state.

--- ttt_train.py
+++ ttt_train.py
@@ -1,4 +1,4 @@
-from ttt_play import Game
+from ttt_play import Game, State
 from ttt_policies import Policy, TabularPolicy
 
 class Train:
@@ -14,13 +14,21 @@ class Train:
           self.SelfPlay()
                
      def SelfPlay(self):
-          """Policy Iteration following Sutton Barto 4.3
+          """Policy Evaluation following Sutton Barto 4.1
+             Against rush opponent
+             with regular states, no afterstates
           """
-          # 1. Initialization
           policy_1 = TabularPolicy(has_v_dict = True)
-          policy_2 = TabularPolicy(has_v_dict = True)
+          policy_2 = RushPolicy()
           
-          # 2. Policy Evaluation
-          delta = 0
+          theta = 0.01
           for _ in range(n_game):
-               
+               delta = 0
+               for num in range(int('1' + '0' * 9, 3), int('2' * 10, 3) + 1):
+                    v = policy_1.v_dict[num]
+                    s = State(from_base10 = num)
+                    opponent_state = policy_1.select_move(s).next_state()
+                    if 
+                    s_prime = policy_2.select_move(opponent_state).next_state()
+                    policy_1.v_dict[num] = 
+                    

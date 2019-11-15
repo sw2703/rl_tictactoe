a9143d1b2649ada5e0fd3be378b7068aebabb259
sw2703 <30507015+sw2703@users.noreply.github.com>
2019-11-14 21:23:37 -0800

    Update ttt_train.py
    TODO: implement the read method.

--- ttt_train.py
+++ ttt_train.py
@@ -1,6 +1,8 @@
 from ttt_play import State
 from ttt_policies import TabularPolicy, RushPolicy
 import numpy as np
+import pickle
+import time
 
 class Train:
      def __init__(self, n_game, write_path, read_path = None):
@@ -23,7 +25,8 @@ class Train:
           policy_2 = RushPolicy()
           
           theta = 0.01
-          for _ in range(self.n_game):
+          t = time.time()
+          for i_game in range(self.n_game):
                delta = 0
                for num in range(int('1' + '0' * 9, 3), int('2' * 10, 3) + 1):
                     v = policy_1.v_dict[num]
@@ -48,5 +51,10 @@ class Train:
                if delta < theta:
                     break
                
+               if time.time() - t > 60:
+                    t = time.time()
+                    print("Trained %i games so far." % i_game)
+                    pickle.dump(policy_1, open(self.write_path, "wb" ) )
+               
 if __name__ == '__main__':
-     Train(n_game = 1, write_path = r'C:\Users\daugh\Documents\GitHub\rl_tictactoe\policy_evaluation.pkl')               
+     Train(n_game = 1000, write_path = r'C:\Users\daugh\Documents\GitHub\rl_tictactoe\policy_evaluation.pkl')               

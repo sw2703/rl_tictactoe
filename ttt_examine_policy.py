5636c4306e57b3b87877926eb329e76724985da4
sw2703 <30507015+sw2703@users.noreply.github.com>
2019-11-16 09:41:46 -0800

    Update ttt_examine_policy.py
    TODO: optimize judge()

--- ttt_examine_policy.py
+++ ttt_examine_policy.py
@@ -7,7 +7,9 @@ Created on Fri Nov 15 19:58:57 2019
 from ttt_play import State
 import pickle
 
-policy, _ = pickle.load(open(r'C:\Users\daugh\Documents\GitHub\rl_tictactoe_data\policy_evaluation.pkl', 'rb'))
+policy, i_epoch = pickle.load(open(r'C:\Users\daugh\Documents\GitHub\rl_tictactoe_data\policy_evaluation.pkl', 'rb'))
+
+print('This value function has been trained for %i epochs.' % i_epoch)
 
 print('End state, expect value 0 even if a player wins.')
 state = State(board = [[1, 1, 1], 
@@ -15,3 +17,20 @@ state = State(board = [[1, 1, 1],
                        [2, 2, 1]])
 print('Value = %f' % policy.v_dict[state.get_num()])
 
+print('One step before a draw. Expect value 0.')
+state = State(board = [[1, 1, 2], 
+                       [2, 2, 1], 
+                       [0, 2, 1]])
+print('Value = %f' % policy.v_dict[state.get_num()])
+
+print('A state from which the only choice is a winning move. Expect value 1.')
+state = State(board = [[1, 1, 0], 
+                       [2, 2, 1], 
+                       [2, 2, 1]])
+print('Value = %f' % policy.v_dict[state.get_num()])
+
+print('One choice among many is a winning move. Expect value 1.')
+state = State(board = [[1, 1, 0], 
+                       [2, 2, 0], 
+                       [0, 0, 0]])
+print('Value = %f' % policy.v_dict[state.get_num()])

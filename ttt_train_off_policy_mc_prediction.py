994c361d25bae9fec5f30726aa93f536cf006ade
sw2703 <30507015+sw2703@users.noreply.github.com>
2019-12-03 09:12:06 -0800

    Update ttt_train_off_policy_mc_prediction.py
    WIP. TODO: check the pseudocode in https://docs.google.com/document/d/1wszVsB3HQUnbmQEeWbv5yw0uxspWyjIHVVK7KqF4P4s/edit

--- ttt_train_off_policy_mc_prediction.py
+++ ttt_train_off_policy_mc_prediction.py
@@ -18,15 +18,13 @@ class Train:
         else:
             self.target_policy = TabularPolicy()
             self.i_epoch = 0
-        self.opponent_policy = TabularPolicy(
-        )  # will be called to give epsilon-soft move with epsilon = 1, i.e. fully random
-        self.behavior_policy = TabularPolicy(
-        )  # behavior policy, will be called to give epsilon-soft move with epsilon = 1, i.e. fully random
+        self.opponent_policy = TabularPolicy(epsilon=1)
+        self.behavior_policy = TabularPolicy(epsilon=1)
         self.path = path
         self.policy_stable = True
         self.epsilon = 0.1
 
-    def TrainContinuously(self, n_epoch=1e24):
+    def TrainContinuously(self, n_epoch=1e99):
         t = time.time()
         while self.i_epoch < n_epoch:
             while time.time() - t < 10 and self.i_epoch < n_epoch:
@@ -43,11 +41,11 @@ class Train:
         # behavior policy playing player 1
         trajectory = self.GetOneTrajectory(
             self.behavior_policy, self.opponent_policy)
-        self.MCPredictIncremental(trajectory)
+        self.MCPredictIncremental(trajectory, 1)
         # behavior policy playing player 2
         trajectory = self.GetOneTrajectory(
             self.opponent_policy, self.behavior_policy)
-        self.MCPredictIncremental(trajectory)
+        self.MCPredictIncremental(trajectory, 2)
 
     def GetOneTrajectory(self, policy_1, policy_2):
         """ 
@@ -58,15 +56,44 @@ class Train:
         while not State(from_base10=num).is_terminal():
             num = policy_1.move(num)
             trajectory.append(num)
+            if not State(from_base10=num).is_terminal():
+                num = policy_2.move(num)
+                trajectory.append(num)
+            else:
+                break
         return trajectory
 
-    def MCPredictIncremental(self, trajectory):
+    def MCPredictIncremental(self, trajectory, role_behavior_policy):
         """ Incremental implementation of off-policy MC prediction
+        Input:
+            trajectory
+            role_behavior_policy: 1 or 2, denoting which player the behavior policy acted as in this trajectory
         """
         c = {}
         # g is a constant for our case
         g = State(from_base10=trajectory[-1]).get_reward()
         w = 1.
+        i = len(trajectory) - 1
+        while i >= 0:
+            if (i % 2 + 1) == role_behavior_policy:
+                if w == 0:
+                    break
+                state = trajectory[i]
+                if state in c:
+                    c[state] += w
+                else:
+                    c[state] = w
+                self.policy_1.v_dict[state] += w / \
+                    c[state] * (g - self.policy_1.v_dict[state])
+                if i != 0 and self.policy_1.move_dict[trajectory[i-1]] != trajectory[i]:
+                    w = 0
+                else:
+                    w = w / \
+                        len(State(from_base10=trajectory[i-1]).legal_afterstates())
+                
+        
+        
+        
         for i, state in reversed(list(enumerate(trajectory))):
             if w == 0:
                 break

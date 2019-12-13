7cc2cb5b160c0487fb396d22a53748c5dfce9517
sw2703 <30507015+sw2703@users.noreply.github.com>
2019-12-12 19:38:18 -0800

    Update ttt_examine_sarsa.py
    also test target policy as player O

--- ttt_examine_sarsa.py
+++ ttt_examine_sarsa.py
@@ -15,6 +15,7 @@ policy, i_epoch = pickle.load(open(os.path.dirname(
 print('This value function has been trained for %i epochs.' % i_epoch)
 theta = 0.01
 print('Accuracy %f' % theta)
+
 opponent_policy = TabularPolicy(epsilon=1)
 results = []
 for i in range(1000):
@@ -29,4 +30,19 @@ for i in range(1000):
                 break
     results.append(state.get_reward())
                 
-print("Average reward %f over 1000 games against random policy." % (sum(results) / 1000.))
+print("Average reward %f over 1000 games as player X against random policy." % (sum(results) / 1000.))
+
+results = []
+for i in range(1000):
+    state = State()
+    while True:
+        state = State(from_base10 = opponent_policy.move(state.get_num()))
+        if state.is_terminal():
+            break
+        else:
+            state = State(from_base10 = policy.move_dict[state.get_num()])
+            if state.is_terminal():
+                break
+    results.append(state.get_reward())
+                
+print("Average reward %f over 1000 games as player O against random policy." % (sum(results) / 1000.))
